ece6c5b0418dd5e3652d46b914cbd6e5a7e0d312
Seema Vora <seema.s.vora@gmail.com>
2021-04-03 21:47:03 -0700

    Adds the Basic BrickBreaker Class and Padding

--- BrickBreakerGame/src/gameReal/BlockBreakerPanel.java
+++ BrickBreakerGame/src/gameReal/BlockBreakerPanel.java
@@ -1,15 +1,21 @@
 package gameReal;
 
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class BlockBreakerPanel extends JPanel implements KeyListener{
 
 	
+	ArrayList<Block> blocks = new ArrayList<Block>();
+	Block paddle = new Block(175,480, 150,25,"paddle.png");
+	Block ball = new Block(237, 435,25,25,"ball.png");
+	
+//	Animation animate;
 	BlockBreakerPanel(){
-		Block paddle = new Block(175,480, 150,25,"paddle.png");
 		
 		addKeyListener(this);
 		setFocusable(true);
@@ -20,6 +26,9 @@ public class BlockBreakerPanel extends JPanel implements KeyListener{
 		
 	}
 
+	public void paintComponent(Graphics g) {
+		paddle.draw(g, this);
+	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -28,6 +37,24 @@ public class BlockBreakerPanel extends JPanel implements KeyListener{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			new Thread(() -> {
+				while(true) {
+					update();
+//					System.out.println("running");
+					try {
+						Thread.sleep(10);
+					}catch(InterruptedException s) {
+					s.printStackTrace();
+					}
+				}
+			}).start();
+			
+
+		}
+		
+	}
+	public void update() {
 		// TODO Auto-generated method stub
 		
 	}
