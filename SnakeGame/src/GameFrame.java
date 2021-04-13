6336213db7688b9df89a50db612d2b2ae4d9529d
Surabhi Gupta <surabhi2029@gmail.com>
2021-04-12 17:03:19 -0700

    Starts basic classes for snakegame

---
+++ SnakeGame/src/GameFrame.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	/** 
+	 * Constructor
+	 */
+	
+	public GameFrame() {
+		
+		GamePanel panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		
+		// Fit JFrame around all other view components
+		this.pack();
+		
+		this.setVisible(true);
+		
+		// Set window in the middle of the screen
+		this.setLocationRelativeTo(null);
+	}
+}
---
+++ SnakeGame/src/GamePanel.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	/** 
+	 * Constructor
+	 */
+	public GamePanel() {
+		
+	}
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions(){
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
+
+}
