d6692ef9aabb3fe2f50722bffb6f4953fd06bc81
Seema Vora <seema.s.vora@gmail.com>
2021-04-25 18:59:06 -0700

    Adds a Basic Game Frame

---
+++ PingPong/src/Paddle.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Paddle extends Rectangle{
+	
+	int id; //either 1 or 2 (depend on player number)
+	int yVelocity; // how fast the paddle is moving
+	
+	Paddle(){
+		
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	
+	public void setYDirection(int yDirection) {
+		
+	}
+	
+	public void move() {
+		
+	}
+
+	public void draw(Graphics g) {
+		
+	}
+}
