6842a00b1abe02893ecc52cf1241ba8124e30807
Seema Vora <seema.s.vora@gmail.com>
2021-04-25 22:02:36 -0700

    Adds Ball

--- PingPong/src/Ball.java
+++ PingPong/src/Ball.java
@@ -9,24 +9,39 @@ public class Ball extends Rectangle{
 	int xVelocity;
 	int yVelocity;
 
-	Ball() {
+	Ball(int x, int y, int width, int height) {
+		super(x,y,width,height);
+		random = new Random();
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0) {
+			randomXDirection--;
+		}
+		setXDirection(randomXDirection);
+		
+		int randomYDirection = random.nextInt(2);
+		if(randomYDirection == 0) {
+			randomYDirection--;
+		}
+		setYDirection(randomYDirection);
 
 	}
 
 	public void setXDirection(int randomXDirection) {
-
+		xVelocity = randomXDirection;
 	}
 
 	public void setYDirection(int randomYDirection) {
-
+		yVelocity = randomYDirection;
 	}
 
 	public void move() {
-
+		x+= xVelocity;
+		y+= yVelocity;
 	}
 	
-	public void draw() {
-		
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillOval(x,y,height, width);
 	}
 
 }
--- PingPong/src/GamePanel.java
+++ PingPong/src/GamePanel.java
@@ -34,6 +34,9 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void newBall() {
+//		random = new Random();
+		ball = new Ball((GAME_WIDTH/2)- (BALL_DIAMETER /2), (GAME_HEIGHT/2)-(BALL_DIAMETER /2), BALL_DIAMETER, BALL_DIAMETER);
+		
 		
 	}
 	
@@ -53,14 +56,29 @@ public class GamePanel extends JPanel implements Runnable{
 	public void draw(Graphics g) {
 		paddle1.draw(g);
 		paddle2.draw(g);
+		ball.draw(g);
 	}
 	
 	public void move() {
-		
+		paddle1.move();
+		paddle2.move();
+		ball.move();
 	}
 	
 	public void checkCollision() {
-		
+		//prevents paddles from going beyond paddle edges
+		if(paddle1.y <= 0) {
+			paddle1.y = 0;
+		}
+		if(paddle1.y >= (GAME_HEIGHT -PADDLE_HEIGHT)) {
+			paddle1.y = GAME_HEIGHT - PADDLE_HEIGHT;
+		}
+		if(paddle2.y <= 0) {
+			paddle2.y = 0;
+		}
+		if(paddle2.y >= (GAME_HEIGHT -PADDLE_HEIGHT)) {
+			paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
+		}
 	}
 	
 	public void run() {
