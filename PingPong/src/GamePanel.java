aeec8e8384a5733c516a0b461f50f51bf9c95234
Seema Vora <seema.s.vora@gmail.com>
2021-04-25 22:36:14 -0700

    DONEEE!!!

--- PingPong/src/GamePanel.java
+++ PingPong/src/GamePanel.java
@@ -34,8 +34,8 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void newBall() {
-//		random = new Random();
-		ball = new Ball((GAME_WIDTH/2)- (BALL_DIAMETER /2), (GAME_HEIGHT/2)-(BALL_DIAMETER /2), BALL_DIAMETER, BALL_DIAMETER);
+		random = new Random();
+		ball = new Ball((GAME_WIDTH/2)- (BALL_DIAMETER /2),random.nextInt(GAME_HEIGHT-BALL_DIAMETER), BALL_DIAMETER, BALL_DIAMETER);
 		
 		
 	}
@@ -57,6 +57,7 @@ public class GamePanel extends JPanel implements Runnable{
 		paddle1.draw(g);
 		paddle2.draw(g);
 		ball.draw(g);
+		score.draw(g);
 	}
 	
 	public void move() {
@@ -66,6 +67,14 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void checkCollision() {
+		//bouncing ball off he edges of the window
+		
+		if(ball.y <= 0) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		if(ball.y >= GAME_HEIGHT-BALL_DIAMETER) {
+			ball.setYDirection(-ball.yVelocity);
+		}
 		//prevents paddles from going beyond paddle edges
 		if(paddle1.y <= 0) {
 			paddle1.y = 0;
@@ -78,6 +87,44 @@ public class GamePanel extends JPanel implements Runnable{
 		}
 		if(paddle2.y >= (GAME_HEIGHT -PADDLE_HEIGHT)) {
 			paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
+		}
+		
+		//bouces ball off paddles
+		if(ball.intersects(paddle1)) {
+			ball.xVelocity = Math.abs(ball.xVelocity);
+			ball.xVelocity++; //makes it more difficult
+			if(ball.yVelocity > 0) {
+				ball.yVelocity++;//makes it more difficult
+			}else {
+				ball.yVelocity--;
+			}
+			ball.setXDirection(ball.xVelocity);
+			ball.setYDirection(ball.yVelocity);
+		}
+		if(ball.intersects(paddle2)) {
+			ball.xVelocity = Math.abs(ball.xVelocity);
+			ball.xVelocity++;
+			if(ball.yVelocity > 0) {
+				ball.yVelocity++;
+			}else {
+				ball.yVelocity--;
+			}
+			ball.setXDirection(-ball.xVelocity);
+			ball.setYDirection(ball.yVelocity);
+		}
+		
+		//give player a 1 point and creates new paddles and ball
+		if(ball.x <= 0) {
+			score.player2++;
+			newPaddles();
+			newBall();
+			System.out.println("Player 2: " + score.player2);
+		}
+		if(ball.x >= GAME_WIDTH-BALL_DIAMETER) {
+			score.player1++;
+			newPaddles();
+			newBall();
+			System.out.println("Player 1: " + score.player1);
 		}
 	}
 	
