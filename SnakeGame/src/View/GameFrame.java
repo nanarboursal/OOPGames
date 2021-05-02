b058d76b8227eb372990be294a10a1b2fa24dd59
Surabhi Gupta <surabhi2029@gmail.com>
2021-05-02 12:19:48 -0700

    fixing model

--- SnakeGame/src/View/GameFrame.java
+++ SnakeGame/src/View/GameFrame.java
@@ -1,25 +1,35 @@
 package View;
-import javax.swing.*;
 
-public class GameFrame extends JFrame{
+import java.awt.FlowLayout;
 
-	/** 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GameFrame extends JFrame {
+
+	/**
 	 * Constructor
 	 */
-	
+
 	public GameFrame() {
-		
+
 		GamePanel panel = new GamePanel();
+		JPanel scorePanel = new JPanel();
+//		int score = panel.getApples();
+//		scorePanel.add(new JLabel("score: " + score));
+//		this.add(scorePanel);
 		this.add(panel);
+
 		this.setTitle("Snake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		
+
 		// Fit JFrame around all other view components
 		this.pack();
-		
+		this.setLayout(new FlowLayout());
+
 		this.setVisible(true);
-		
 		// Set window in the middle of the screen
 		this.setLocationRelativeTo(null);
 	}
